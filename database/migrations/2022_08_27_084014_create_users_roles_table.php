5dad64c976a00cfb5f30eb4d8163a11f02d482c5
coding-master15 <codingmaster2004@gmail.com>
2022-08-27 13:56:44 +0500

    hello

---
+++ database/migrations/2022_08_27_084014_create_users_roles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_roles', function (Blueprint $table) {
+            $table->integer('user_id');
+            $table->integer('role_id');
+
+         //SETTING THE PRIMARY KEYS
+           $table->primary(['user_id','role_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_roles');
+    }
+};
