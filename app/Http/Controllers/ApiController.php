414ad9fd2c34180318f131fa4c9bc8a83ddea16d
coding-master15 <codingmaster2004@gmail.com>
2022-09-17 17:00:54 +0500

    wdaw

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -158,7 +158,7 @@ class ApiController extends Controller
 
         $data = array('customer'=> $value);
         \Mail::send('emails.register', $data, function($message) {
-            $message->to($customer->email, $customer->fname.' '.$customer->lname)->subject
+            $message->to($value->email, $value->fname.' '.$value->lname)->subject
                 ('Thanks for Joining Shubh Chintak');
             $message->from('theshubhchintaq@gmail.com','Shubh Chintak');
         });
