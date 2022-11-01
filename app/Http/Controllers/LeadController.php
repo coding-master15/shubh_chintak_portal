6bbc18a2fae52890499c01c472ccb1881af99e4d
coding-master15 <codingmaster2004@gmail.com>
2022-11-01 19:29:39 +0500

    hello

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -104,14 +104,14 @@ class LeadController extends Controller
                 return Datatables::of($data)
                     ->addIndexColumn()
                     ->addColumn('action', function($row){
-                      $btn = '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                      $btn = '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalw'.$row->id.'">
                       View
                     </button>';
-                      $btn.='<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                      $btn.='<div class="modal fade" id="exampleModalw'.$row->id.'" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabelw'.$row->id.'" aria-hidden="true">
                       <div class="modal-dialog" role="document">
                         <div class="modal-content">
                           <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">'.$row->title.'</h5>
+                            <h5 class="modal-title" id="exampleModalLabelw'.$row->id.'">'.$row->title.'</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                               <span aria-hidden="true">&times;</span>
                             </button>
