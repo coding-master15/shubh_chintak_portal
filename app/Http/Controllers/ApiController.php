88000dec812b385b0dc74ba47d1dd3060cb446be
My Name <codingmaster2004@gmail.com>
2023-02-20 03:02:03 +0500

    a

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -140,6 +140,7 @@ class ApiController extends Controller
             'product_type' => $this->request->input('product_type'),
             'service_type' => $this->request->input('service_type'),
             'advice' => $this->request->input('advice') == 'true' ? 1 : 0,
+
             'type' => $this->request->input('type'),
             'budget' => $this->request->input('budget'),
             'area' => $this->request->input('area'),
